a819bfc9bfd151e6c37ec9bc8fa425a23e3db832
c0deb0t <daniel.liu02@gmail.com>
2022-12-10 22:13:33 -0800

    Move Job structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::{path::PathBuf, time::Duration};
-
 use serde::{Deserialize, Serialize};
 
 /// The non-negative cost of an alignment.
@@ -83,41 +81,4 @@ pub enum CostModel {
         /// > 0
         extend: Cost,
     },
-}
-
-/// An alignment job: a single task for the runner to execute and benchmark.
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Job {
-    /// Path to a `.seq` file.
-    pub dataset: PathBuf,
-    /// The cost model to use.
-    pub costs: CostModel,
-    /// Return the full alignment/cigar?
-    pub traceback: bool,
-
-    /// The algorithm/parameters to use.
-    pub algo: Algorithm,
-}
-
-/// The output of an alignment job.
-#[derive(Serialize, Deserialize, Debug)]
-pub struct JobOutput {
-    pub runtime: Duration,
-    pub memory: Bytes,
-    pub costs: Vec<Cost>,
-    pub cigars: Vec<Cigar>,
-}
-
-/// The result of an alignment job, containing the input and output.
-#[derive(Serialize, Deserialize, Debug)]
-pub struct JobResult {
-    pub job: Job,
-    pub output: Option<JobOutput>,
-}
-
-/// Which algorithm to run and benchmark, along with algorithm-specific parameters.
-#[derive(Serialize, Deserialize, Debug)]
-pub enum Algorithm {
-    BlockAligner { min_size: usize, max_size: usize },
-    // Add more algorithms here!
 }
